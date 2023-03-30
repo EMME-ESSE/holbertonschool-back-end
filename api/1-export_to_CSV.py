0a4a12fd754790e34bf91d84c7c7e4e38c8d49e7
EMME-ESSE <spiritexdi@gmail.com>
2023-03-30 17:17:06 -0300

    .

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""comments"""
 import csv
 import requests
 import sys
 
 
 if __name__ == '__main__':
+    """comment"""
     if len(sys.argv) < 2:
         print("Usage: ./2-export_to_CSV.py employee_id")
         sys.exit(1)
