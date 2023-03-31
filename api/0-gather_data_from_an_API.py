fada9b4818e09e73bdfaacca793d74de700a18e3
EMME-ESSE <spiritexdi@gmail.com>
2023-03-31 11:48:29 -0300

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,15 +2,16 @@
 """comments"""
 import requests
 
-def get_employee_todo_progress(employee_id):
+if __name__ == '__main__':
     """Commented"""
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
-    data = response.json()
-    total_tasks = len(data)
-    completed_tasks = sum(1 for task in data if task["completed"])
-    employee_name = data[0]["name"].split(":")[0]
-    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
-    for task in data:
-        if task["completed"]:
-            print(f"\t- {task['title']}")
-get_employee_todo_progress(1)
+    def get_employee_todo_progress(employee_id):
+        response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+        data = response.json()
+        total_tasks = len(data)
+        completed_tasks = sum(1 for task in data if task["completed"])
+        employee_name = data[0]["name"].split(":")[0]
+        print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+        for task in data:
+            if task["completed"]:
+                print(f"\t- {task['title']}")
+    get_employee_todo_progress(1)
