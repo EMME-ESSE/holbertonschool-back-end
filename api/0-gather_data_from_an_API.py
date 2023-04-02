6cfb450803f1392ddda19624db8f9e6025978b84
EMME-ESSE <spiritexdi@gmail.com>
2023-04-02 10:18:57 -0300

    fixing errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-"""Commenting"""
-from requests import get
+"""Comments"""
+import requests
 from sys import argv
 
 
-def get_api():
-    """Comments"""
-    emp_id = int(argv[1])
-    emp_name = ''
-    tasks_done = 0
-    tasks_total = 0
-    tasks_titles = []
-
-    users_res = get('https://jsonplaceholder.typicode.com/users').json()
-    for user in users_res:
-        if user['id'] == emp_id:
-            emp_name = user['name']
-            break
-
-    tasks_res = get('https://jsonplaceholder.typicode.com/todos').json()
-    for task in tasks_res:
-        if task['userId'] == emp_id:
-            if task['completed']:
-                tasks_titles.append(task['title'])
-                tasks_done += 1
-            tasks_total += 1
-
-    print('Employee {} is done with tasks({}/{}):'.format(emp_name,
-                                                          tasks_done,
-                                                          tasks_total))
-    for title in tasks_titles:
-        print('\t {}'.format(title))
+def gather_data():
+    """Commenting"""
+    user_data = int(argv[1])
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(user_data))
+    par_user = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(user_data))
+    todos_json = todos.json()
+    completed_tasks, tasks, task_list = 0, 0, []
+    for t in todos_json:
+        if t['completed'] is True:
+            completed_tasks += 1
+            task_list.append("\t {}".format(t['title']))
+        tasks += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(par_user['name'], completed_tasks, tasks))
+    print(*task_list, sep='\n')
 
 
 if __name__ == '__main__':
-    get_api()
+    gather_data()
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,25 +5,23 @@ import requests
 from sys import argv
 
 
-def get_api():
+def gather_data_csv():
     """Comments"""
-    url = 'https://jsonplaceholder.typicode.com/'
-    uid = argv[1]
-
-    usr = requests.get(url + 'users/{}'.format(uid)).json()
-    todo = requests.get(url + 'todos', params={'userId': uid}).json()
-
-    with open('{}.csv'.format(uid), 'w') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for employee in todo:
-            user_id = uid
-            username = usr.get('username')
-            task_comp = employee.get('completed')
-            task_title = employee.get('title')
-
-            emp_record = [user_id, username, task_comp, task_title]
-            writer.writerow(emp_record)
+    user_data = int(argv[1])
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(user_data))
+    par_user = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(user_data))
+    todos_json = todos.json()
+    task_list = []
+    for t in todos_json:
+        task_list.append([user_data, par_user['username'], t['completed'],
+                         t['title']])
+    with open('{}.csv'.format(user_data), 'w+', encoding='UTF8') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(task_list)
 
 
 if __name__ == '__main__':
-    get_api()
+    gather_data_csv()
